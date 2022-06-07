1d523cbda2ce02c00f6e91f99d31c9143129a063
Sean Pathasema <spathasema@gmail.com>
2022-06-07 13:22:14 -0500

    removed icons

--- src/MvpCard.js
+++ src/MvpCard.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea, ListItem } from '@mui/material';
-import { margin, styled } from '@mui/system';
-import CheckBoxTwoToneIcon from '@mui/icons-material/CheckBoxTwoTone';
-import './MvpCard.css'
-import zIndex from '@mui/material/styles/zIndex';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, ListItem } from "@mui/material";
+import { margin, styled } from "@mui/system";
+import CheckBoxTwoToneIcon from "@mui/icons-material/CheckBoxTwoTone";
+import "./MvpCard.css";
+import zIndex from "@mui/material/styles/zIndex";
 
 const ArtworkCard = styled(Card)({
   height: "270px",
@@ -15,13 +15,12 @@ const ArtworkCard = styled(Card)({
   textAlign: "center",
 });
 
-
 const ThumbImage = styled(CardMedia)({
-  height: '150px',
-  width: 'auto',
-  marginTop: '20px',
-  marginLeft: 'auto',
-  marginRight: 'auto'
+  height: "150px",
+  width: "auto",
+  marginTop: "20px",
+  marginLeft: "auto",
+  marginRight: "auto",
 });
 
 const CheckBox = styled(CheckBoxTwoToneIcon)({
@@ -29,36 +28,34 @@ const CheckBox = styled(CheckBoxTwoToneIcon)({
   paddingTop: "230px",
   paddingLeft: "60px",
   fontSize: "2em",
-  zIndex: "99"
-})
-
-
+  zIndex: "99",
+});
 
 export default function MvpCard(props) {
-
   const handleSelection = () => {
-    console.log("in handleselect")
-    props.select()
-  }
-
+    console.log("in handleselect");
+    props.select();
+  };
 
   return (
-    <ArtworkCard elevation={props.id === props.choice ? 2 : 10} className={props.id === props.choice ? 'userSelected' : ''}>
-      {props.id === props.choice ? <CheckBox/> : '' }
-    <CardActionArea onClick={handleSelection}>
-      <ThumbImage
-        component="img"
-        height="140"
-        image={`/static/images/${props.img}`}
-        alt={props.title}
-      />
-      <CardContent>
-      
-        <Typography gutterBottom variant="subtitle2" component="div">
-        {props.id === props.choice ? `${props.title}` : `${props.title}` }
-        </Typography>
-      </CardContent>
-    </CardActionArea>
-  </ArtworkCard>
-  )
+    <ArtworkCard
+      elevation={props.id === props.choice ? 2 : 10}
+      className={props.id === props.choice ? "userSelected" : ""}
+    >
+      {props.id === props.choice ? <CheckBox /> : ""}
+      <CardActionArea onClick={handleSelection}>
+        <ThumbImage
+          component="img"
+          height="140"
+          image={`/static/images/${props.img}`}
+          alt={props.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="subtitle2" component="div">
+            {props.id === props.choice ? `${props.title}` : `${props.title}`}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </ArtworkCard>
+  );
 }
