edb97498c355ce44015aa6588ffb000c6541c273
Sean Pathasema <spathasema@gmail.com>
2022-06-08 17:14:39 -0500

    loading card color change

--- src/Loading.js
+++ src/Loading.js
@@ -4,6 +4,7 @@ import Card from '@mui/material/Card';
 import Box from "@mui/material/Box";
 import Skeleton from "@mui/material/Skeleton";
 import { Typography } from "@mui/material";
+import { SportsRugbySharp } from "@mui/icons-material";
 
 export default function Loading() {
   return (
@@ -21,7 +22,7 @@ export default function Loading() {
     <Grid container wrap="wrap" spacing={2} columns={12} sx={{ pl: 1, pt: 10 }} style={{ padding: "35px 0 0 22px", margin: 0}}>
       {Array.from(new Array(12)).map((item, index) => (
          <Grid key={`grid-item-${index}`} item xs={3} style={{ marginBottom: 34}}>
-         <Card key={`card-${index}`} style={{ height: 267, width: 188, paddingTop: 28, boxSizing: "border-box" }}>    
+         <Card key={`card-${index}`} style={{ height: 267, width: 188, paddingTop: 28, boxSizing: "border-box", backgroundColor: 'rgba(255, 555, 255, 0.1)' }}>    
          <Skeleton
            style={{ marginBottom: 10, margin:"0 auto"}}
            variant="rectangular"
